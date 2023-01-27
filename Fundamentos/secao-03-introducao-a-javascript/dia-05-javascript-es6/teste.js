c7d6046f54c1ec785a191006e480561a7fb3dfd9
Severino Vinicius <severino.sales95@gmail.com>
2023-01-27 20:09:19 -0300

    Exercicios JS dia 05

---
+++ Fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6/teste.js
@@ -0,0 +1,10 @@
+
+
+// Com o template literals
+console.log(`Primeira linha;
+Segunda linha;
+Terceira linha;`
+);
+
+// Sem o template literals:
+console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n');
